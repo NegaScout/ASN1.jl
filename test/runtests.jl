36e29b8f97b6775357f8a872b53dfe6a20958fa2
Jan Wagner <jenik.wagner@gmail.com>
2025-04-07 21:16:20 +0200

    initial implementation + tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 using JET
 
 @testset "ASN1.jl" begin
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(ASN1; target_defined_modules = true)
-    end
-    # Write your tests here.
+#    @testset "Code linting (JET.jl)" begin
+#        JET.test_package(ASN1; target_defined_modules = true)
+#    end
+    include("ber.jl")
 end
